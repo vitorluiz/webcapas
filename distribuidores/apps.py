0621ada9d234959f5be8d562a3457433375b6834
vitorluiz <vitorluizmachado@gmail.com>
2024-06-22 10:33:13 -0400

    config: criando apps e defiindo lang pt-br

---
+++ distribuidores/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DistribuidoresConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'distribuidores'
---
+++ ponto_venda/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PontoVendaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ponto_venda'
